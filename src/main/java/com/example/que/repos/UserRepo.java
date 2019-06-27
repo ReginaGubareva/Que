c14d5863876ea633f34aa76de3ccac8f9e3aa951
Regina Gubareva <rengubareva@gmail.com>
2019-06-27 12:08:56 +0600

    Fix registration and log in. It's not clear what was wrong.

---
+++ src/main/java/com/example/que/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.que.repos;
+
+import com.example.que.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
